bdd16b480e5c2c6f77dbe8ff408a390f1a58875a
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-15 15:10:09 +0100

    update

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -2,7 +2,42 @@ import React from "react";
 import styled from "styled-components";
 
 const Header = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      {/* <Wrapper>
+        <Logo />
+        <Middle>
+          <Span>
+            men
+            <Icon>
+              <HiOutlineChevronDown />
+            </Icon>
+          </Span>
+          <Span>women</Span>
+          <Span>beauty</Span>
+          <Span>sport</Span>
+          <Span>
+            templates
+            <Icon>
+              <HiOutlineChevronDown />
+            </Icon>
+          </Span>
+          <Span>
+            explore
+            <Icon>
+              <HiOutlineChevronDown />
+            </Icon>
+          </Span>
+        </Middle>
+        <Right>
+            <SearchIcon>
+                <FiSearch />
+            </SearchIcon>
+
+        </Right>
+      </Wrapper> */}
+    </Container>
+  );
 };
 
 export default Header;
--- src/Components/Routes/Routes.tsx
+++ src/Components/Routes/Routes.tsx
@@ -1,15 +1,36 @@
-import React from 'react'
-import { useRoutes } from 'react-router'
+import React from "react";
+import { useRoutes } from "react-router";
+import Explore from "../Explore/Explore";
+import Men from "../Men/Men";
+import Sport from "../Sport/Sport";
+import Templates from "../Templates/Templates";
+import Women from "../Women/Women";
 
 const Routes = () => {
+  const element = useRoutes([
+    {
+      path: "/",
+      element: <Men />,
+    },
+    {
+      path: "/women",
+      element: <Women />,
+    },
+    {
+      path: "/beauty",
+      element: <Sport />,
+    },
+    {
+      path: "/templates",
+      element: <Templates />,
+    },
+    {
+      path: "/explore",
+      element: <Explore />,
+    },
+  ]);
 
-    const element = useRoutes([
-        {
-            path: "/",
-        }
-    ])
-    
-  return element
-}
+  return element;
+};
 
-export default Routes
+export default Routes;
