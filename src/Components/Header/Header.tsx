fbd612aae45f676a5e4ac01f2b9f6c2c920146d3
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-15 15:38:38 +0100

    update

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import styled from "styled-components";
+import { HiOutlineChevronDown } from "react-icons/hi";
+import { FiSearch } from "react-icons/fi";
+import { BsCartDash } from "react-icons/bs";
+import { RxPerson } from "react-icons/rx";
+import logo from "../Images/logo.png";
 
 const Header = () => {
   return (
     <Container>
-      {/* <Wrapper>
-        <Logo />
+      <Wrapper>
+        <Logo src={logo} />
         <Middle>
           <Span>
             men
@@ -30,12 +35,17 @@ const Header = () => {
           </Span>
         </Middle>
         <Right>
-            <SearchIcon>
-                <FiSearch />
-            </SearchIcon>
-
+          <IconWrap>
+            <FiSearch />
+          </IconWrap>
+          <IconWrap>
+            <RxPerson />
+          </IconWrap>
+          <AddToCart>
+            <BsCartDash />
+          </AddToCart>
         </Right>
-      </Wrapper> */}
+      </Wrapper>
     </Container>
   );
 };
@@ -45,5 +55,43 @@ export default Header;
 const Container = styled.div`
   height: 70px;
   width: 100%;
-  background-color: red;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
+
+const Wrapper = styled.div`
+  width: 90%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Logo = styled.img`
+  object-fit: cover;
+  height: 40px;
+`;
+
+const Middle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Span = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 20px;
+`;
+const Icon = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 3px;
+  margin-left: 6px;
+`;
+const Right = styled.div``;
+const IconWrap = styled.div``;
+const AddToCart = styled.div``;
