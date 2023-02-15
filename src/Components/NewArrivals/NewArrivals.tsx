e942c4f9fb947132f3cafd0c891fcc321f9ff5fc
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-15 18:33:57 +0100

    update

---
+++ src/Components/NewArrivals/NewArrivals.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const NewArrivals = () => {
+  return <Container>NewArrivals</Container>;
+};
+
+export default NewArrivals;
+
+const Container = styled.div`
+  margin-top: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
