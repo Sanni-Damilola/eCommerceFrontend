ffcdf02a6c373f164842148060f6fce7a1610b3f
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-15 16:08:50 +0100

    update

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -104,9 +104,11 @@ const Right = styled.div`
 `;
 const IconWrap = styled.div`
   margin-right: 20px;
-  font-size: 25px;
+  font-size: 20px;
+  color: rgb(0, 0, 0, 0.6);
 `;
 const AddToCart = styled.div`
   font-size: 20px;
   margin-right: 10px;
+  color: rgb(0, 0, 0, 0.6);
 `;
--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,13 +1,46 @@
 import React from "react";
 import styled from "styled-components";
+import { RiSearch2Line } from "react-icons/ri";
+import img from "../Images/hero1.png";
 
 const Hero = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <First>
+        <Title>hero In this season, find the best 🔥</Title>
+        <Heading>Exclusive collection for everyone</Heading>
+        <Button>
+          Explore
+          <RiSearch2Line />
+        </Button>
+      </First>
+      <Second src={img} />
+    </Container>
+  );
 };
 
 export default Hero;
 
+const Second = styled.img`
+  object-fit: contain;
+  width: 650px;
+`;
+
 const Container = styled.div`
   width: 100%;
-  /* height: ; */
+  height: 500px;
+  background-color: #e3ffe6;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
+
+const Button = styled.button``;
+const Heading = styled.div`
+  font-size: 40px;
+  font-weight: 900;
+`;
+const Title = styled.div`
+  font-size: 25px;
+`;
+const First = styled.div``;
