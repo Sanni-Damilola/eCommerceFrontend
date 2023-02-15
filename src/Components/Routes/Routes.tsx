9a0871b8c8f0ac45b932af72cca882bcc92a8cce
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-15 17:07:51 +0100

    update

--- src/Components/Routes/Routes.tsx
+++ src/Components/Routes/Routes.tsx
@@ -2,33 +2,9 @@ import React from "react";
 import { useRoutes } from "react-router";
 import Explore from "../Explore/Explore";
 import HomeScreen from "../Home/HomeScreen";
-import Men from "../Men/Men";
-import Sport from "../Sport/Sport";
-import Templates from "../Templates/Templates";
-import Women from "../Women/Women";
 
 const Routes = () => {
   const element = useRoutes([
-    // {
-    //   path: "/",
-    //   element: <Men />,
-    // },
-    // {
-    //   path: "/women",
-    //   element: <Women />,
-    // },
-    // {
-    //   path: "/beauty",
-    //   element: <Sport />,
-    // },
-    // {
-    //   path: "/templates",
-    //   element: <Templates />,
-    // },
-    // {
-    //   path: "/explore",
-    //   element: <Explore />,
-    // },
     {
       path: "/",
       element: <HomeScreen />,
