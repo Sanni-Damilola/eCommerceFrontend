0adec3534723f7c740713bc309dfeb6c978a08ed
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-16 20:24:59 +0100

    update

--- src/Components/allRoute/allRoutes.tsx
+++ src/Components/allRoute/allRoutes.tsx
@@ -2,19 +2,13 @@ import React from "react";
 import { useRoutes } from "react-router-dom";
 import HomeComp from "../Home/homeComp";
 
-
 export const AllRoute = () => {
+  const element = useRoutes([
+    {
+      path: "/",
+      element: <HomeComp />,
+    },
+  ]);
 
-    const element = useRoutes([
-        {
-            path: "/",
-            element: <HomeComp /> 
-        },
-        // {
-        //     path: "/details",
-        //     element: <Detail /> 
-        // },
-    ])
-
-    return element
-} 
+  return element;
+};
