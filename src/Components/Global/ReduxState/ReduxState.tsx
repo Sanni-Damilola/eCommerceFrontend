51a232dde1bead3cc5a8d3a0da3bb3d00cdd5a4a
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-17 20:51:53 +0100

    update

--- src/Components/Global/ReduxState/ReduxState.tsx
+++ src/Components/Global/ReduxState/ReduxState.tsx
@@ -1,13 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction } from "@reduxjs/toolkit/dist/createAction";
+
+interface userData {
+  name: string;
+  email: string;
+  password: string;
+}
 
 const initialState = {
-    
+  currentUser: {} || null,
 };
 
 const ReduxState = createSlice({
   name: "second",
   initialState,
-  reducers: {},
+  reducers: {
+    loginUser: (state, { payload }: PayloadAction<userData>) => {},
+  },
 });
 
 export const {} = ReduxState.actions;
