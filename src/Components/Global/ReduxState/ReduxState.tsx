044ed438b5d98df4b796ad8a023fc4c40474e51d
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-17 20:29:38 +0100

    update

---
+++ src/Components/Global/ReduxState/ReduxState.tsx
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const ReduxState = createSlice({
+  name: "second",
+  initialState,
+  reducers: {},
+});
+
+export const {} = ReduxState.actions;
+
+export default ReduxState.reducer;
