0b88bf18476ab98511d1d6a8e7cef6e1ec6c72b5
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-23 15:34:48 +0100

    update

--- src/Components/Ship/Ship.tsx
+++ src/Components/Ship/Ship.tsx
@@ -1,33 +1,37 @@
 import React from "react";
 import styled from "styled-components";
+import step1 from "../Images/step1.png"
+import step2 from "../Images/step2.png"
+import step3 from "../Images/step3.png"
+import step4 from "../Images/step4.png"
 
 const Ship = () => {
   return (
     <Container>
       <Holder>
         <Card>
-          <Image src="" />
+          <Image src={step1} />
           <Step>Step1</Step>
           <Title>Filter & Discover</Title>
           <Desc>Smart filtering and suggestions make it easy to find</Desc>
         </Card>
         <Card>
-          <Image src="" />
-          <Step>Step1</Step>
-          <Title>Filter & Discover</Title>
-          <Desc>Smart filtering and suggestions make it easy to find</Desc>
+          <Image src={step2} />
+          <Step>Step2</Step>
+          <Title>Add to bag</Title>
+          <Desc>Easily select the correct items and add them to the cart</Desc>
         </Card>
         <Card>
-          <Image src="" />
+          <Image src={step3} />
           <Step>Step1</Step>
-          <Title>Filter & Discover</Title>
-          <Desc>Smart filtering and suggestions make it easy to find</Desc>
+          <Title>Fast shipping</Title>
+          <Desc>The carrier will confirm and ship quickly to you</Desc>
         </Card>
         <Card>
-          <Image src="" />
-          <Step>Step1</Step>
-          <Title>Filter & Discover</Title>
-          <Desc>Smart filtering and suggestions make it easy to find</Desc>
+          <Image src={step4} />
+          <Step>Step4</Step>
+          <Title>Enjoy the product</Title>
+          <Desc>Have fun and enjoy your 5-star quality products</Desc>
         </Card>
       </Holder>
     </Container>
